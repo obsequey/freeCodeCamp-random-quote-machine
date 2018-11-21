e29b0781c8c56981efccb52c72b2e52271cd767e
Artyom Aristov <obsequey@protonmail.com>
2018-11-21 18:49:59 +0300

    add gitignore and babelrc

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    h3 {{ data }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: 'Here will be something amazing'
+    }
+  }
+}
+</script>
