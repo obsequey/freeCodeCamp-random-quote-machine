d4ccc6087c8df8f8264b8f8580ecbefb8c29b0bb
Artyom Aristov <dei40752@gmail.com>
2019-02-01 12:33:58 +0300

    style a little

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  div#app
-    h1.f1 ⭐ Quote Maker v0.1 ⭐
-    button(v-on:click="getNewQuote()") Generate quote
-    blockquote#quote "{{ quote.saying }}"
-    label(for="quote") - {{ quote.author }}
+  div.flex.flex-column.items-center
+    h1.f1.pv4 Quote Maker v0.1
+    blockquote#quote.h4.f3.pv3.ph5.lh-title {{ quote.saying }}
+    label(for="quote").h3.f4.pv3 - {{ quote.author }}
+    button(v-on:click="getNewQuote()").w5.h2.mv3.pointer.shadow-4 Generate quote
+    a(href="https://breaking-bad-quotes.herokuapp.com").f7.link.black.hover-gray More quotes ~
 </template>
 
 <script>
@@ -22,60 +23,13 @@ export default {
     getNewQuote() {
       axios({
         method: 'get',
-        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
+        url: 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
       })
       .then(response => {
-        this.quote.saying = response.data.quote
-        this.quote.author = response.data.author
+        this.quote.saying = response.data[0].quote
+        this.quote.author = response.data[0].author
       })
     }
   }
 }
 </script>
-
-<style lang="scss">
-  #app {
-    // font-size: 14px;
-    position: relative;
-    width: 75%;
-    margin: 2rem auto;
-    font-family: 'niramit', serif;
-  }
-  h1 {
-    font-weight: 100;
-    font-size: 1.3rem;
-    text-align: center;
-  }
-  blockquote {
-    font-family: 'niramit', serif;
-    width: auto;
-    margin: 4rem auto 3rem auto;
-    text-align: left;
-    display: block;
-    font-size: 35px;
-  }
-  label {
-    font-family: 'roboto', serif;
-    display: block;
-    position: absolute;
-    right: 10;
-    font-size: 20px;
-  }
-  button {
-    font-family: 'Roboto', sans-serif;
-    display: block;
-    margin: 3rem auto;
-    width: auto;
-    background-color: lavender;
-    border: none;
-    font-size: 15px;
-    padding: 12px 15px;
-    border-radius: 5px;
-    box-shadow: 0 20px 15px -10px rgba(0, 0, 0, 0.3);
-    transition: .3s all ease-in-out;
-    &:active {
-      bottom: 200;
-      transform: scale(0.95);
-    }
-  }
-</style>
